c8245d27aded9abd2b6ad81ac0ef6dbd118af8cf
Benoit Hamelin <benoit@benoithamelin.com>
2020-10-04 14:17:23 -0400

    Bump version to 2.0.1
    Signed-off-by: Benoit Hamelin <benoit@benoithamelin.com>

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     entry_points={
         "console_scripts": ["pyh = helphelp:main"]
     },
-    version="2.0",
+    version="2.0.1",
     description="man-like tool to get online help on Python modules, classes and functions",
     long_description=long_description,
     long_description_content_type="text/markdown",
